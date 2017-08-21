790ff62f829e5eb505deeadce21929463bceb579
Jonathan Steele <ynnoj@users.noreply.github.com>
2017-08-21 09:04:09 +0100

    feat: Make CSS variables configurable in app config
    Fixes #58

--- index.js
+++ index.js
@@ -10,16 +10,7 @@ const faPath = path.dirname(require.resolve('font-awesome/package.json'));
 module.exports = {
   name: 'ember-cli-notifications',
 
-  options: {
-    cssModules: {
-      postcssOptions: {
-        map: true
-      },
-      plugins: [
-        require('postcss-cssnext')
-      ]
-    }
-  },
+  options: {},
 
   treeForVendor() {
     return new Funnel(faPath, {
@@ -32,8 +23,6 @@ module.exports = {
     const projectConfig = this.project.config(app.env);
     const config = projectConfig['ember-cli-notifications'];
 
-    this._super.included.apply(this, arguments);
-
     // see: https://github.com/ember-cli/ember-cli/issues/3718
     if (typeof app.import !== 'function' && app.app) {
       app = app.app;
@@ -41,10 +30,35 @@ module.exports = {
 
     this.app = app;
 
+    this.assignCssVariables(config);
+
     // Don't import Font Awesome assets if specified in consuming app
     if (config.includeFontAwesome !== false) {
       this.importFontAwesome(app);
     }
+
+    this._super.included.apply(this, arguments);
+  },
+
+  assignCssVariables(config) {
+    this.options = Object.assign({}, this.options, {
+      cssModules: {
+        plugins: [
+          require('postcss-cssnext')
+        ],
+        postcssOptions: {
+          map: true
+        },
+        virtualModules: {
+          'colour-variables': {
+            '--error-colour': config.errorColour || '#e74c3c',
+            '--info-colour': config.infoColour || '#3ea2ff',
+            '--success-colour': config.successColour || '#64ce83',
+            '--warn-colour': config.warnColour || '#ff7f48',
+          }
+        }
+      }
+    });
   },
 
   importFontAwesome(app) {
